bf1b38287852fe5baa2ecec4feecbb615edfbdd5
Antonio Fechita <antonio.fechita@yahoo.com>
2022-05-15 00:06:15 +0300

    replaced if chain with switch statement + simplified an if statement

--- src/main/java/chessboard/pieces/ChessPiece.java
+++ src/main/java/chessboard/pieces/ChessPiece.java
@@ -86,101 +86,80 @@ public abstract class ChessPiece
 
     public static ChessPiece getChessPieceFromCode(char code)
     {
-        if (code == 'K')
+        switch (code)// == 'K')
         {
-            King whiteKing = new King();
-            whiteKing.setPieceCode(code);
-            whiteKing.setPieceColor(ChessColor.WHITE);
-            return whiteKing;
-        }
-
-        if (code == 'k')
-        {
-            King blackKing = new King();
-            blackKing.setPieceCode(code);
-            blackKing.setPieceColor(ChessColor.BLACK);
-            return blackKing;
-        }
-
-        if (code == 'Q')
-        {
-            Queen whiteQueen = new Queen();
-            whiteQueen.setPieceCode(code);
-            whiteQueen.setPieceColor(ChessColor.WHITE);
-            return whiteQueen;
-        }
-
-        if (code == 'q')
-        {
-            Queen blackQueen = new Queen();
-            blackQueen.setPieceCode(code);
-            blackQueen.setPieceColor(ChessColor.BLACK);
-            return blackQueen;
-        }
-
-        if (code == 'R')
-        {
-            Rook whiteRook = new Rook();
-            whiteRook.setPieceCode(code);
-            whiteRook.setPieceColor(ChessColor.WHITE);
-            return whiteRook;
-        }
-
-        if (code == 'r')
-        {
-            Rook blackRook = new Rook();
-            blackRook.setPieceCode(code);
-            blackRook.setPieceColor(ChessColor.BLACK);
-            return blackRook;
-        }
-
-
-        if (code == 'N')
-        {
-            Knight whiteKnight = new Knight();
-            whiteKnight.setPieceCode(code);
-            whiteKnight.setPieceColor(ChessColor.WHITE);
-            return whiteKnight;
-        }
-
-        if (code == 'n')
-        {
-            Knight blackKnight = new Knight();
-            blackKnight.setPieceCode(code);
-            blackKnight.setPieceColor(ChessColor.BLACK);
-            return blackKnight;
-        }
-
-        if (code == 'B')
-        {
-            Bishop whiteBishop = new Bishop();
-            whiteBishop.setPieceCode(code);
-            whiteBishop.setPieceColor(ChessColor.WHITE);
-            return whiteBishop;
-        }
-
-        if (code == 'b')
-        {
-            Bishop blackBishop = new Bishop();
-            blackBishop.setPieceCode(code);
-            blackBishop.setPieceColor(ChessColor.BLACK);
-            return blackBishop;
-        }
-
-        if (code == 'P')//capital P
-        {
-            Pawn whitePawn = new Pawn();
-            whitePawn.setPieceCode(code);
-            whitePawn.setPieceColor(ChessColor.WHITE);
-            return whitePawn;
-        }
-
-        if (code == 'p')//small p
-        {
-            Pawn blackPawn = new Pawn();
-            blackPawn.setPieceCode(code);
-            blackPawn.setPieceColor(ChessColor.BLACK);
-            return blackPawn;
+            case 'K' -> {
+                King whiteKing = new King();
+                whiteKing.setPieceCode(code);
+                whiteKing.setPieceColor(ChessColor.WHITE);
+                return whiteKing;
+            }
+            case 'k' -> {
+                King blackKing = new King();
+                blackKing.setPieceCode(code);
+                blackKing.setPieceColor(ChessColor.BLACK);
+                return blackKing;
+            }
+            case 'Q' -> {
+                Queen whiteQueen = new Queen();
+                whiteQueen.setPieceCode(code);
+                whiteQueen.setPieceColor(ChessColor.WHITE);
+                return whiteQueen;
+            }
+            case 'q' -> {
+                Queen blackQueen = new Queen();
+                blackQueen.setPieceCode(code);
+                blackQueen.setPieceColor(ChessColor.BLACK);
+                return blackQueen;
+            }
+            case 'R' -> {
+                Rook whiteRook = new Rook();
+                whiteRook.setPieceCode(code);
+                whiteRook.setPieceColor(ChessColor.WHITE);
+                return whiteRook;
+            }
+            case 'r' -> {
+                Rook blackRook = new Rook();
+                blackRook.setPieceCode(code);
+                blackRook.setPieceColor(ChessColor.BLACK);
+                return blackRook;
+            }
+            case 'N' -> {
+                Knight whiteKnight = new Knight();
+                whiteKnight.setPieceCode(code);
+                whiteKnight.setPieceColor(ChessColor.WHITE);
+                return whiteKnight;
+            }
+            case 'n' -> {
+                Knight blackKnight = new Knight();
+                blackKnight.setPieceCode(code);
+                blackKnight.setPieceColor(ChessColor.BLACK);
+                return blackKnight;
+            }
+            case 'B' -> {
+                Bishop whiteBishop = new Bishop();
+                whiteBishop.setPieceCode(code);
+                whiteBishop.setPieceColor(ChessColor.WHITE);
+                return whiteBishop;
+            }
+            case 'b' -> {
+                Bishop blackBishop = new Bishop();
+                blackBishop.setPieceCode(code);
+                blackBishop.setPieceColor(ChessColor.BLACK);
+                return blackBishop;
+            }
+            case 'P' -> {
+                Pawn whitePawn = new Pawn();
+                whitePawn.setPieceCode(code);
+                whitePawn.setPieceColor(ChessColor.WHITE);
+                return whitePawn;
+            }
+            case 'p' -> {
+                Pawn blackPawn = new Pawn();
+                blackPawn.setPieceCode(code);
+                blackPawn.setPieceColor(ChessColor.BLACK);
+                return blackPawn;
+            }
         }
 
         return null;
@@ -216,9 +195,6 @@ public abstract class ChessPiece
         if (targetField == null)
             return false;
 
-        if (targetField.getChessPiece() == null || targetField.getChessPiece().getPieceColor() != pieceColor)
-            return true;
-
-        return false;
+        return targetField.getChessPiece() == null || targetField.getChessPiece().getPieceColor() != pieceColor;
     }
 }
